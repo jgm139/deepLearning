9b79c84937a3b929903f56004bf4e35b759ff2a9
RollitoDelicioso <reg16@alu.ua.es>
2017-11-14 13:48:31 +0100

    88.66% cambio de activación a relu y tamaño de 30 a 40

--- homus_cnn.py
+++ homus_cnn.py
@@ -33,22 +33,19 @@ def load_data():
     #
     # Load data from data/HOMUS/train_0, data/HOMUS/train_1,...,data/HOMUS_31 folders from HOMUS images
     #
-    image_list = [] # array de todas las imágenes de todas las carpetas train_**
-    class_list = [] # array con las clases de cada imagen del array image_list
+    image_list = []
+    class_list = []
     for current_class_number in range(0,nb_classes):    # Number of class
-        for filename in glob.glob('./data/HOMUS/train_{}/*.jpg'.format(current_class_number)): # cogemos la carpeta con la clase actual
-            im = load_img(filename, grayscale=True, target_size=[img_rows, img_cols])  # cargamos cada imagen de la carpeta
-            image_list.append(np.asarray(im).astype('float32')/255) # añadimos la imagen al array image_list[]
-            class_list.append(current_class_number) # y su clase correspondiente en la misma posición
+        for filename in glob.glob('./data/HOMUS/train_{}/*.jpg'.format(current_class_number)):
+            im = load_img(filename, grayscale=True, target_size=[img_rows, img_cols])  # this is a PIL image
+            image_list.append(np.asarray(im).astype('float32')/255)
+            class_list.append(current_class_number)
 
-    n = len(image_list)    # Total ejemplos
-    
-    # asarray = Convierte a array([1, 2]) el input
-    # reshape = Redimensiona a un nuevo tamaño un array sin cambiar los datos, puede recibir un int o una tupla de enteros
-    X = np.asarray(image_list).reshape(n, img_rows, img_cols, 1) # array de n imágenes, cada una dividida entre sus filas y columnas
-    input_shape = (img_rows, img_cols, 1) # tamaño de cada imagen
-    
-    # to_categorical() = Convierte un vector de enteros a una matriz binaria
+    n = len(image_list)    # Total examples
+
+    X = np.asarray(image_list).reshape(n, img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
     Y = np_utils.to_categorical(np.asarray(class_list), nb_classes)
 
     msk = np.random.rand(len(X)) < 0.9 # Train 90% and Test 10%
@@ -63,19 +60,14 @@ def cnn_model(input_shape):
     # LeNet-5: Artificial Neural Network Structure
     #
 
-    model = Sequential() # red neuronal de tipo secuencial
-    
-    # añadimos a la red una convolución 2D (producto escalar entre la ventana de la imagen y el filtro)
-    # filters = 6 // número de salidas
-    # kernel_size (6, 6)
-    # padding = valid
-    # data_format = input_shape
+    model = Sequential()
+
     model.add(Conv2D(6, (6, 6), padding='valid', input_shape = input_shape))
-    model.add(Activation("sigmoid"))    #función de activación sigmoid
-    model.add(MaxPooling2D(pool_size=(2, 2))) #votación máxima
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Conv2D(16, (5, 5), padding='valid'))
-    model.add(Activation("sigmoid"))
+    model.add(Activation("relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.5))
 
@@ -83,7 +75,7 @@ def cnn_model(input_shape):
 
     model.add(Flatten())
     model.add(Dense(1024))
-    model.add(Activation("sigmoid"))
+    model.add(Activation("relu"))
     model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
 
@@ -105,7 +97,7 @@ print(epochs, 'epochs')
 model = cnn_model(input_shape)
 print(model.summary())
 
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 early_stopping = EarlyStopping(monitor='loss', patience=3)
 model.fit(X_train, Y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_data=(X_test, Y_test), callbacks=[early_stopping])
