7737b30d60360501e9f4ac704b4bf50818955e09
RollitoDelicioso <reg16@alu.ua.es>
2017-11-15 11:52:07 +0100

    91.14%

--- homus_cnn.py
+++ homus_cnn.py
@@ -65,10 +65,14 @@ def cnn_model(input_shape):
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3)))
+    model.add(Conv2D(16, (6, 6)))
+    model.add(Activation('relu'))
+
+
+    model.add(Conv2D(16, (6, 6)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.2))
 
     model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
