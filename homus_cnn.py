e7a8e304242ed325757c9a1192a5699914bc69a9
Julia García Martínez <jgm139@alu.ua.es>
2017-11-23 23:40:36 +0100

    93.13% 30/40 épocas

--- homus_cnn.py
+++ homus_cnn.py
@@ -68,7 +68,7 @@ def cnn_model(input_shape):
     model.add(Conv2D(32, (3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.3))
+    model.add(Dropout(0.5))
 
     model.add(Conv2D(64, (3, 3)))
     model.add(Activation('relu'))
